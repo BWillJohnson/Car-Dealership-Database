9eeeed4797022669d7798be14d61f46f40cc4084
Brandon <brandonjohnson.bj880@gmail.com>
2024-11-26 08:36:58 -0500

    Completing ReadMe.

---
+++ Challenge 6,2.0.sql
@@ -0,0 +1,6 @@
+SELECT *
+FROM sales_contract sc
+INNER JOIN inventory i ON i.Vin = sc.Vin
+INNER JOIN Dealership d ON i.dealershipID = d.dealershipID
+WHERE DATE >= '2024-08-01'
+ORDER BY sc.CustomerLastName;
